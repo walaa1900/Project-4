709a73b211d188c3f481c809edbc1859fdb00b85
rosamp1 <132237292+rosamp1@users.noreply.github.com>
2023-12-05 21:09:34 -0600

    ERD added

---
+++ QuickDBD-export ERD.sql
@@ -0,0 +1,22 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Patient" (
+    "id" int   NOT NULL,
+    "gender" varchar   NOT NULL,
+    "age" int   NOT NULL,
+    "hypertension" int   NOT NULL,
+    "heart_disease" int   NOT NULL,
+    "ever_married" varchar   NOT NULL,
+    "work_type" varchar   NOT NULL,
+    "Residence_type" varchar   NOT NULL,
+    "avg_glucose_level" float   NOT NULL,
+    "bmi" float   NOT NULL,
+    "smoking_status" varchar   NOT NULL,
+    "stroke" int   NOT NULL,
+    CONSTRAINT "pk_Patient" PRIMARY KEY (
+        "id"
+     )
+);
+
